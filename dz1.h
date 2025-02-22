5feba11bce9601a6cf994afa500207ac13469051
korolev <kirillkorolev0510@gmail.com>
2025-02-22 12:47:53 +0300

    Initial commit

---
+++ dz1.h
@@ -0,0 +1,10 @@
+//
+// Created by KOROLEV Alexandr on 22.02.2025.
+//
+
+#ifndef DZ1_H
+#define DZ1_H
+
+void dz1(); // Объявление функции
+
+#endif //DZ1_H
---
+++ dz2.h
@@ -0,0 +1,10 @@
+//
+// Created by KOROLEV Alexandr on 22.02.2025.
+//
+
+#ifndef DZ2_H
+#define DZ2_H
+
+void dz2(); // Объявление функции
+
+#endif //DZ2_H
